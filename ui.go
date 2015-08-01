5c2dd927a28e9ab04497a0a8650bfb71a56be4fc
apokalyptik <apokalyptik@apokalyptik.com>
2015-08-01 14:38:55 +0000

    include fonts

--- ui.go
+++ ui.go
@@ -2,4 +2,4 @@ package main
 
 //go:generate sass $PWD/www/css/style.scss $PWD/www/css/style.css
 //go:generate browserify -t reactify -t uglifyify $PWD/www/js/app/main.jsx -o $PWD/www/js/production.js
-//go:generate $GOPATH/bin/go-bindata -ignore=$PWD/www/css/.*.scss -prefix $PWD/ $PWD/www/ $PWD/www/js $PWD/www/css
+//go:generate $GOPATH/bin/go-bindata -ignore=$PWD/www/css/.*.scss -prefix $PWD/ $PWD/www/ $PWD/www/js $PWD/www/css $PWD/www/fonts
