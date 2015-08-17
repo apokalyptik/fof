8c8bddd043b5b558ed01bcf11ac9cadca9cd97e4
apokalyptik <apokalyptik@apokalyptik.com>
2015-08-17 03:25:25 +0000

    no longer recording whole json... no longer storing net data in memberDestinyStats

--- micro/services/destiny-user-stats/user.go
+++ micro/services/destiny-user-stats/user.go
@@ -3,11 +3,9 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
+
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 	"sync"
@@ -95,58 +93,28 @@ type user struct {
 	data     map[string]*userBit
 }
 
-func (u *user) set(what string, info interface{}, err error) {
-	if _, ok := u.data[what]; !ok {
-		u.data[what] = &userBit{}
-	}
-	if err != nil {
-		log.Printf("Error fetching %s for %s: %s", what, u.name, err.Error())
-		return
-	}
-	u.data[what].data = info
-	u.data[what].when = time.Now()
-}
-
 func (u *user) cUpsert(collection string, query, data interface{}) error {
 	_, err := mgoDB.DB("fof").C(collection).Upsert(query, data)
 	return err
 }
 
 func (u *user) pull() error {
-	defer func() {
-		var doc = map[string]interface{}{}
-		for k, v := range u.data {
-			doc[k] = v.data
-			delete(u.data, k)
+	var search = []struct {
+		MembershipType int    `mapstructure:"membershipType"`
+		MembershipId   string `mapstructure:"membershipId"`
+	}{}
+	// Try XBL First
+	info, buf, err := client.get(playerURL(platformXBL, u.name))
+	if err != nil {
+		return err
+	} else {
+		if err := mapstructure.Decode(info, &search); err != nil {
+			return err
 		}
-		if bytes, err := json.Marshal(doc); err != nil {
-			log.Printf("Error marshalling data for %s: %s", u.name, err.Error())
-		} else {
-			err = ioutil.WriteFile(fmt.Sprintf("data/%d.%s.json", u.platform, u.account), bytes, 0644)
-			if err != nil {
-				log.Printf("Error writing datafile for user data/%d.%s.json: %s", u.platform, u.account, err.Error())
-			}
-		}
-		c := mgoDB.DB("fof").C("memberDestinyStats")
-		_, err := c.Upsert(bson.M{"member": u.name}, &struct {
-			Member string
-			Data   map[string]interface{}
-		}{
-			Member: u.name,
-			Data:   doc,
-		})
-		if err != nil {
-			log.Printf("Error inserting document into MongoDB: %s", err.Error())
-		}
-	}()
-
-	if u.platform == 0 {
-		var search = []struct {
-			MembershipType int    `mapstructure:"membershipType"`
-			MembershipId   string `mapstructure:"membershipId"`
-		}{}
-		// Try XBL First
-		info, buf, err := client.get(playerURL(platformXBL, u.name))
+	}
+	if len(search) < 1 {
+		// Try PSN Second
+		info, buf, err = client.get(playerURL(platformPSN, u.name))
 		if err != nil {
 			return err
 		} else {
@@ -154,62 +122,35 @@ func (u *user) pull() error {
 				return err
 			}
 		}
-		if len(search) < 1 {
-			// Try PSN Second
-			info, buf, err = client.get(playerURL(platformPSN, u.name))
-			if err != nil {
-				return err
-			} else {
-				if err := mapstructure.Decode(info, &search); err != nil {
-					return err
-				}
-			}
-		}
-		if len(search) < 1 {
-			return errUserNotFound
-		}
-		u.cUpsert(
-			"userData",
-			bson.M{
-				"account": u.account,
-				"name":    u.name,
-				"type":    "player",
-			},
-			map[string]interface{}{
-				"username": u.userName,
-				"userid":   u.userID,
-				"account":  u.account,
-				"name":     u.name,
-				"type":     "player",
-				"data":     info.([]interface{})[0],
-				"raw":      buf,
-			},
-		)
-		u.platform = search[0].MembershipType
-		u.account = search[0].MembershipId
-		u.data["player"] = &userBit{
-			when: time.Now(),
-			data: info.([]interface{})[0],
-		}
 	}
-
-	if bytes, err := ioutil.ReadFile(fmt.Sprintf("data/%d.%s.json", u.platform, u.account)); err != nil {
-		if !os.IsNotExist(err) {
-			log.Printf("Error reading %s: %s", fmt.Sprintf("data/%d.%s.json", u.platform, u.account), err.Error())
-		}
-	} else {
-		var doc = map[string]interface{}{}
-		if err := json.Unmarshal(bytes, &doc); err != nil {
-			log.Printf("Error unmarshalling data for %d/%s (%s): %s", u.platform, u.account, u.name, err.Error())
-		} else {
-			for k, v := range doc {
-				u.set(k, v, nil)
-			}
-		}
+	if len(search) < 1 {
+		return errUserNotFound
+	}
+	u.cUpsert(
+		"userData",
+		bson.M{
+			"account": u.account,
+			"name":    u.name,
+			"type":    "player",
+		},
+		map[string]interface{}{
+			"username": u.userName,
+			"userid":   u.userID,
+			"account":  u.account,
+			"name":     u.name,
+			"type":     "player",
+			"data":     info.([]interface{})[0],
+			"raw":      buf,
+		},
+	)
+	u.platform = search[0].MembershipType
+	u.account = search[0].MembershipId
+	u.data["player"] = &userBit{
+		when: time.Now(),
+		data: info.([]interface{})[0],
 	}
 
 	astats, buf, err := client.get(accountStatsURL(u.platform, u.account))
-	u.set("accountStats", astats, err)
 	if err == nil {
 		u.cUpsert(
 			"userData",
@@ -259,7 +200,6 @@ func (u *user) pull() error {
 			},
 		)
 	}
-	u.set("grimoire", grim, err)
 
 	t, buf, err := client.get(triumphsURL(u.platform, u.account))
 	if err == nil {
@@ -281,7 +221,6 @@ func (u *user) pull() error {
 			},
 		)
 	}
-	u.set("triumphs", t, err)
 
 	account, buf, err := client.get(accountURL(u.platform, u.account))
 	if err == nil {
@@ -303,7 +242,6 @@ func (u *user) pull() error {
 			},
 		)
 	}
-	u.set("account", account, err)
 
 	if err != nil {
 		// todo... decide how to handle error handing in this function...
@@ -410,7 +348,6 @@ func (u *user) pull() error {
 			}
 		}
 	}
-	u.set("characters", characterDoc, nil)
 
 	// Merge into single document
 	return nil
