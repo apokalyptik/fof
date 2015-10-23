8e8879d684f554f085d44c7494ec198f3ad0de45
bcherian <bearcherian@gmail.com>
2015-10-22 22:41:09 -0400

    Making Select App more Bootstrapy with a button dropdown. Adding componentDidMount() to clear Dispatcher errors and set appname when the dropdown is changed.

--- tools/team/www/js/app/hello/select-an-app.jsx
+++ tools/team/www/js/app/hello/select-an-app.jsx
@@ -4,19 +4,31 @@ Dispatcher = require('../lib/dispatcher.jsx');
 module.exports = SelectAnApp = React.createClass({
 	render: function() {
 		var viewing = this.props.viewing;
+		var lfgNow = "LFG Now";
+		var lfgLater = "LFG Later";
+		var buttonText = viewing == "events" ? lfgLater : lfgNow;
 		return (
-			<select id="select-an-app" defaultValue={viewing} onChange={
-				function(event) {
-					Dispatcher.dispatch({
-						actionType: "set",
-						key: "viewing",
-						value: event.target.value
-					});
-				}}>
-				<option value="events">LFG Later</option>
-				<option value="lfg">LFG Now</option>
-			</select>
+			<div className="btn-group selectApp">
+				<button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<span className="glyphicon glyphicon-th-list" aria-hidden="true"></span><span className="hidden-xs">&nbsp;{buttonText}</span>
+				</button>
+				<ul className="dropdown-menu">
+					<li><a href="#" data-value="events">{lfgLater}</a></li>
+					<li><a href="#" data-value="lfg">{lfgNow}</a></li>
+				</ul>
+			</div>
 		);
+	},
+	componentDidMount: function() {
+		var viewing = this.props.viewing;
+		$(".selectApp ul li a").bind("click tap",function(e){
+			e.preventDefault();
+			appName = $(e.target).data("value");
+			Dispatcher.dispatch({ actionType: "set", key: "error", value: "" });
+			Dispatcher.dispatch({ actionType: "set", key: "viewing", value: appName });
+
+		});
+		
 	}
 });
 
