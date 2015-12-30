f6e68bfa5d56c820ace30a8bc8e2aeecd728dc4e
Demitrious Kelly <apokalyptik@apokalyptik.com>
2015-12-30 10:05:31 -0800

    env config for api key, loop param

--- tools/old-file-purge/main.go
+++ tools/old-file-purge/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/dustin/go-humanize"
@@ -14,8 +15,9 @@ import (
 var (
 	templateURL    = "https://slack.com/api/files.list?token=%s&page=%d"
 	templateDelete = "https://slack.com/api/files.delete?token=%s&file=%s"
-	apiKey         = ""
+	apiKey         = os.Getenv("APIKEY")
 	maxAge         = 672 * time.Hour
+	looping        = time.Duration(0)
 )
 
 type fileListResponse struct {
@@ -34,66 +36,73 @@ type fileListResponse struct {
 func init() {
 	flag.StringVar(&apiKey, "api", apiKey, "slack API Key")
 	flag.DurationVar(&maxAge, "max", maxAge, "maximum age of files to keep")
+	flag.DurationVar(&looping, "loop", looping, "how long to wait before running again. 0 to run only once")
 }
 
 func main() {
 	flag.Parse()
 	newest := time.Now().Add(0 - maxAge).Unix()
-	var kept uint64
-	var pruned uint64
-	var unknown uint64
-	for i := 0; i < 1000; i++ {
-		if rsp, err := http.Get(fmt.Sprintf(templateURL, apiKey, i)); err != nil {
-			log.Fatal(err)
-		} else {
-			var data fileListResponse
-			var dec = json.NewDecoder(rsp.Body)
-			if err := dec.Decode(&data); err != nil {
+	for {
+		var kept uint64 = 0
+		var pruned uint64 = 0
+		var unknown uint64 = 0
+		for i := 0; i < 1000; i++ {
+			if rsp, err := http.Get(fmt.Sprintf(templateURL, apiKey, i)); err != nil {
 				log.Fatal(err)
-			}
-			rsp.Body.Close()
-			if !data.OK {
-				log.Fatalf("Error fetching list: %s", data.Error)
-			}
-			if data.Files == nil {
-				break
-			}
-			if len(data.Files) == 0 {
-				break
-			}
-			for _, file := range data.Files {
-				if file.Created >= newest {
-					kept = kept + file.Size
-					log.Println(
-						"skipping",
-						humanize.Bytes(file.Size),
-						file.MimeType,
-						humanize.Time(time.Unix(file.Timestamp, 0)),
-						fmt.Sprintf("'%s'", file.Name))
-					continue
+			} else {
+				var data fileListResponse
+				var dec = json.NewDecoder(rsp.Body)
+				if err := dec.Decode(&data); err != nil {
+					log.Fatal(err)
 				}
-				if d, err := http.Get(fmt.Sprintf(templateDelete, apiKey, file.ID)); err != nil {
-					unknown = unknown + file.Size
-					log.Println(
-						"delete failure",
-						humanize.Bytes(file.Size),
-						file.MimeType,
-						humanize.Time(time.Unix(file.Timestamp, 0)),
-						fmt.Sprintf("'%s'", file.Name),
-						fmt.Sprintf("'%s'", err.Error()),
-					)
-				} else {
-					pruned = pruned + file.Size
-					log.Println(
-						"delete success",
-						humanize.Bytes(file.Size),
-						file.MimeType,
-						humanize.Time(time.Unix(file.Timestamp, 0)),
-						fmt.Sprintf("'%s'", file.Name))
-					d.Body.Close()
+				rsp.Body.Close()
+				if !data.OK {
+					log.Fatalf("Error fetching list: %s", data.Error)
+				}
+				if data.Files == nil {
+					break
+				}
+				if len(data.Files) == 0 {
+					break
+				}
+				for _, file := range data.Files {
+					if file.Created >= newest {
+						kept = kept + file.Size
+						log.Println(
+							"skipping",
+							humanize.Bytes(file.Size),
+							file.MimeType,
+							humanize.Time(time.Unix(file.Timestamp, 0)),
+							fmt.Sprintf("'%s'", file.Name))
+						continue
+					}
+					if d, err := http.Get(fmt.Sprintf(templateDelete, apiKey, file.ID)); err != nil {
+						unknown = unknown + file.Size
+						log.Println(
+							"delete failure",
+							humanize.Bytes(file.Size),
+							file.MimeType,
+							humanize.Time(time.Unix(file.Timestamp, 0)),
+							fmt.Sprintf("'%s'", file.Name),
+							fmt.Sprintf("'%s'", err.Error()),
+						)
+					} else {
+						pruned = pruned + file.Size
+						log.Println(
+							"delete success",
+							humanize.Bytes(file.Size),
+							file.MimeType,
+							humanize.Time(time.Unix(file.Timestamp, 0)),
+							fmt.Sprintf("'%s'", file.Name))
+						d.Body.Close()
+					}
 				}
 			}
 		}
+		log.Println("kept", humanize.Bytes(kept), "deleted", humanize.Bytes(pruned), "unknown", humanize.Bytes(unknown))
+		if int64(looping) < 1 {
+			break
+		}
+		time.Sleep(looping)
 	}
-	log.Println("kept", humanize.Bytes(kept), "deleted", humanize.Bytes(pruned), "unknown", humanize.Bytes(unknown))
 }
