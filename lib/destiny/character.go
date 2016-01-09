8f10a22d13f0be2741186eb2a5d21d0df3b70c48
Demitrious Kelly <apokalyptik@apokalyptik.com>
2016-01-09 13:20:28 -0800

    continued work on the destiny api client...

---
+++ lib/destiny/character.go
@@ -0,0 +1,26 @@
+package destiny
+
+type Character struct {
+	id string
+	a  *Account
+}
+
+func (c *Character) ActivityHistory() (*Request, error) {
+	return c.a.ActivityHistory(c.id)
+}
+
+func (c *Character) CharacterSummary() (*Request, error) {
+	return c.a.CharacterSummary(c.id)
+}
+
+func (c *Character) CharacterActivities() (*Request, error) {
+	return c.a.CharacterActivities(c.id)
+}
+
+func (c *Character) CharacterInventory() (*Request, error) {
+	return c.a.CharacterInventory(c.id)
+}
+
+func (c *Character) CharacterProgression() (*Request, error) {
+	return c.a.CharacterProgression(c.id)
+}
--- lib/destiny/client.go
+++ lib/destiny/client.go
@@ -3,11 +3,20 @@ package destiny
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"sync"
 	"time"
 )
+
+var (
+	ErrDestinyAccountNotFound = fmt.Errorf("We were unable to find your Destiny account information. If you have a valid Destiny Account, let us know.")
+)
+
+var errorMap = map[int]error{
+	1601: ErrDestinyAccountNotFound,
+}
 
 // Platform Definitions
 const (
@@ -27,7 +36,6 @@ type responseEnvelope struct {
 	Message         string
 	ThrottleSeconds int64
 	Response        *json.RawMessage
-	// MessageData  interface{}
 }
 
 // Client provides an interface to BungieNet.Platform.DestinyServices
@@ -37,32 +45,142 @@ type Client struct {
 	client    *http.Client
 	baseURL   string
 	userAgent string
+	debugURL  bool
 	wait      *time.Time
 	lock      sync.Mutex
 }
 
+// Request is the basic work unit of this API. It allows you to select
+// the API endpoint that you desire and also add url parameters to the
+// request and then finally make the request and get the data back out
 type Request struct {
 	*url.URL
 	url.Values
 	c *Client
 }
 
+// ToURL returns the request url as a string
 func (r *Request) ToURL() string {
-	return fmt.Sprintf("%s%s?%s", r.c.baseURL, r.URL.String()[1:], r.Values.Encode())
+	if v := r.Values.Encode(); v != "" {
+		return fmt.Sprintf("%s%s?%s", r.c.baseURL, r.URL.String()[1:], v)
+	}
+	return fmt.Sprintf("%s%s", r.c.baseURL, r.URL.String()[1:])
 }
 
+// DebugInto works as Into except it enables logging of the fetched URL
+// to stderr
+func (r *Request) DebugInto(into interface{}) error {
+	r.c.debugURL = true
+	err := r.Into(into)
+	r.c.debugURL = false
+	return err
+}
+
+// Into makes the request and unmarshals the response in one of three ways
+// depending on the format of the response...
+//
+// First if there is a ->Response->data (as in CharacterSummary) Response->data
+// will be unmarshalled into the data structure supplied
+//
+// Second if there is a ->Response->results
+// (as in GetAdminsOfGroup ( http://bungienetplatform.wikia.com/wiki/GetAdminsOfGroup ) )
+// then Response->results will be unmarshalled into the data structure supplied.
+// Note there may not be any of these kinds of api calles added to this client as of yes
+// but when they are this is here to ensure that they integrate smoothly
+//
+// Lastly if there is nither a ->data nor a ->results (as in SearchDestinyPlayer)
+// then the ->Response itself will be unmarshalled into the provided data structure
+//
+// Example:
+//		type bungieSearchResponse []struct {
+//			MembershipId string `json:"membershipId"`
+//		}
+//		var d bungieSearchResponse
+//		req, _ := destinyClient.SearchDestinyPlayer(destiny.PlatformXBL, user.GamerTag)
+//		_ := req.Into(&d)
+//		if len(d) {
+//			user.DestinyID = d[0].MembershipId
+//		}
 func (r *Request) Into(into interface{}) error {
-	return r.c.get(r.URL.String()[1:]+"?"+r.Values.Encode(), into)
+	if v := r.Values.Encode(); v != "" {
+		return r.c.get(r.URL.String()[1:]+"?"+v, into)
+	}
+	return r.c.get(r.URL.String()[1:], into)
 }
 
+// CustomURL Allows you to input any URL you desire into the client and make a request against it
+// in this way you can use API endpoints not yet added to the client. Also the helper endpoints
+// all use this one function internally.
+func (c *Client) CustomURL(URL string) (*Request, error) {
+	u, e := url.ParseRequestURI(URL)
+	return &Request{URL: u, c: c, Values: url.Values{}}, e
+}
+
+// SearchDestinyPlayer returns a list of Destiny memberships given a full Gamertag or PSN ID.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=SearchDestinyPlayer%2f%7bmembershipType%7d%2f%7bdisplayName%7d%2f
+// see: http://bungienetplatform.wikia.com/wiki/SearchDestinyPlayer
+func (c *Client) SearchDestinyPlayer(platform int, gamertag string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/SearchDestinyPlayer/%d/%s/", platform, gamertag))
+}
+
+// AccountSummary returns Destiny account information for the supplied membership
+// in a compact summary form.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fSummary%2f
+// see: http://bungienetplatform.wikia.com/wiki/GetDestinyAccountSummary
 func (c *Client) AccountSummary(platform int, id string) (*Request, error) {
-	u, e := url.ParseRequestURI(fmt.Sprintf("/%d/Account/%s/Summary/", platform, id))
-	return &Request{URL: u, c: c, Values: url.Values{}}, e
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Summary/", platform, id))
 }
 
+// AccountItems returns information about all items on the for the supplied
+// Destiny Membership ID, and a minimal set of character information so that
+// it can be used.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fItems%2f
+func (c *Client) AccountItems(platform int, id string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Items/", platform, id))
+}
+
+// ActivityHistory returns activity history stats for indicated character.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=Stats%2fActivityHistory%2f%7bmembershipType%7d%2f%7bdestinyMembershipId%7d%2f%7bcharacterId%7d%2f
+// see: http://bungienetplatform.wikia.com/wiki/GetActivityHistory
 func (c *Client) ActivityHistory(platform int, id string, cid string) (*Request, error) {
-	u, e := url.ParseRequestURI(fmt.Sprintf("/Stats/ActivityHistory/%d/%s/%s/", platform, id, cid))
-	return &Request{URL: u, c: c, Values: url.Values{}}, e
+	return c.CustomURL(fmt.Sprintf("/Stats/ActivityHistory/%d/%s/%s/", platform, id, cid))
+}
+
+// CharacterSummary returns a character summary for the supplied membership.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fCharacter%2f%7bcharacterId%7d%2f
+// see: http://bungienetplatform.wikia.com/wiki/GetCharacterSummary
+func (c *Client) CharacterSummary(platform int, id string, cid string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Character/%s/", platform, id, cid))
+}
+
+// CharacterActivities returns activity progress for a given character.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fCharacter%2f%7bcharacterId%7d%2fActivities%2f
+// see: http://bungienetplatform.wikia.com/wiki/GetCharacterActivities
+func (c *Client) CharacterActivities(platform int, id string, cid string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Character/%s/Activities/", platform, id, cid))
+}
+
+// CharacterInventory returns summary information for the inventory for the supplied character.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fCharacter%2f%7bcharacterId%7d%2fInventory%2fSummary%2f
+func (c *Client) CharacterInventory(platform int, id string, cid string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Character/%s/Inventory/Summary/", platform, id, cid))
+}
+
+// CharacterProgression returns the progression details for the supplied character.
+// see: https://www.bungie.net/platform/destiny/help/HelpDetail/GET?uri=%7bmembershipType%7d%2fAccount%2f%7bdestinyMembershipId%7d%2fCharacter%2f%7bcharacterId%7d%2fProgression%2f
+// see: http://bungienetplatform.wikia.com/wiki/GetCharacterProgression
+func (c *Client) CharacterProgression(platform int, id string, cid string) (*Request, error) {
+	return c.CustomURL(fmt.Sprintf("/%d/Account/%s/Character/%s/Progression/", platform, id, cid))
+}
+
+// Platform returns a platform client which will obviate the need to provide
+// platform distinctions for helper methods (convenient when you're only going
+// to be working against one platform)
+func (c *Client) Platform(platformID int) *Platform {
+	return &Platform{
+		c:  c,
+		id: platformID,
+	}
 }
 
 func (c *Client) get(uri string, into interface{}) error {
@@ -72,7 +190,9 @@ func (c *Client) get(uri string, into interface{}) error {
 		c.wait = nil
 	}
 	// Prepare our request
-	//log.Println(fmt.Sprintf("%s%s", c.baseURL, uri))
+	if c.debugURL {
+		log.Println(fmt.Sprintf("destiny.Client debug: %s%s", c.baseURL, uri))
+	}
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", c.baseURL, uri), nil)
 	c.lock.Unlock()
 	if err != nil {
@@ -99,12 +219,29 @@ func (c *Client) get(uri string, into interface{}) error {
 
 	// Check for failure
 	if envelope.ErrorCode != errorSuccess {
+		if e, ok := errorMap[envelope.ErrorCode]; ok {
+			return e
+		}
 		return fmt.Errorf("API returned error code %d: %s %s", envelope.ErrorCode, envelope.ErrorStatus, envelope.Message)
 	}
 
 	if envelope.ThrottleSeconds > 0 {
 		t := time.Now().Add(time.Duration(envelope.ThrottleSeconds) * time.Second)
+		log.Println("Destiny API Servers requested throttling:", (time.Duration(envelope.ThrottleSeconds) * time.Second).String())
 		c.wait = &t
+	}
+
+	var possibleResponse struct {
+		Data    *json.RawMessage `json:"data,omitempty"`
+		Results *json.RawMessage `json:"results,omitempty"`
+	}
+	if err := json.Unmarshal(*envelope.Response, &possibleResponse); err == nil {
+		if possibleResponse.Data != nil {
+			return json.Unmarshal(*possibleResponse.Data, &into)
+		}
+		if possibleResponse.Results != nil {
+			return json.Unmarshal(*possibleResponse.Results, &into)
+		}
 	}
 
 	return json.Unmarshal(*envelope.Response, &into)
