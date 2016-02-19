e5c09f7f5fb00ef6d6ea228a287bc3914c541f9d
apokalyptik <apokalyptik@apokalyptik.com>
2016-02-19 04:10:59 +0000

    hopefully get around influxdb client timeouts... not super gracefull... will lose a message...

--- micro/services/slack-activity-stats/influx.go
+++ micro/services/slack-activity-stats/influx.go
@@ -10,53 +10,57 @@ import (
 var infxMessage = make(chan slackmsg, 1024)
 
 func mindInfluxDB() {
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: infxAddr,
-	})
-	if err != nil {
-		log.Fatalf("Error creating influxdb client: %s", err.Error())
-	}
-	go func(c client.Client) {
-		defer c.Close()
-		bpCount := 0
-		bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
-			Database:  "fof",
-			Precision: "s",
-		})
-
+	go func() {
+		var c client.Client
+		defer func(c client.Client) { c.Close() }(c)
 		for {
-			select {
-			case message := <-infxMessage:
-				pt, err := client.NewPoint(
-					"public_mesages",
-					map[string]string{
-						"user_name":    message.User,
-						"user_id":      message.UserID,
-						"channel_name": message.Channel,
-						"channel_id":   message.ChannelID,
-					},
-					map[string]interface{}{
-						"message": message.Text,
-					},
-					message.time(),
-				)
-				if err != nil {
-					log.Fatalf("error in client.NewPoint: %s", err.Error())
-				}
-				bp.AddPoint(pt)
-				bpCount++
-				fmt.Println(pt.String())
-				if bpCount >= 1 {
-					if err := c.Write(bp); err != nil {
-						log.Fatalf("error in client.Client.Write: %s", err.Error())
+			if cl, err := client.NewHTTPClient(client.HTTPConfig{Addr: infxAddr}); err != nil {
+				log.Fatalf("Error creating influxdb client: %s", err.Error())
+			} else {
+				c = cl
+			}
+			bpCount := 0
+			bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+				Database:  "fof",
+				Precision: "s",
+			})
+
+			for {
+				select {
+				case message := <-infxMessage:
+					pt, err := client.NewPoint(
+						"public_mesages",
+						map[string]string{
+							"user_name":    message.User,
+							"user_id":      message.UserID,
+							"channel_name": message.Channel,
+							"channel_id":   message.ChannelID,
+						},
+						map[string]interface{}{
+							"message": message.Text,
+						},
+						message.time(),
+					)
+					if err != nil {
+						log.Printf("error in client.NewPoint: %s", err.Error())
+						break
 					}
-					bpCount = 0
-					bp, _ = client.NewBatchPoints(client.BatchPointsConfig{
-						Database:  "fof",
-						Precision: "s",
-					})
+					bp.AddPoint(pt)
+					bpCount++
+					fmt.Println(pt.String())
+					if bpCount >= 1 {
+						if err := c.Write(bp); err != nil {
+							log.Printf("error in client.Client.Write: %s", err.Error())
+							break
+						}
+						bpCount = 0
+						bp, _ = client.NewBatchPoints(client.BatchPointsConfig{
+							Database:  "fof",
+							Precision: "s",
+						})
+					}
 				}
 			}
 		}
-	}(c)
+	}()
 }
